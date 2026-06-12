1d2d307fa39bdaa7bc261cc58fbc4a0cdc2942de
seannguyen255 <dangnh2505@gmail.com>
2026-06-12 18:40:24 +0700

    buoi 9

--- Hospital_Project_full_reactjs_csharp/hospital_FE/src/services/api.js
+++ Hospital_Project_full_reactjs_csharp/hospital_FE/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Xử lý API URL
 // export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5247/api';
-export const API_BASE_URL = 'https://api-hospital.cybersoft.edu.vn/api';
+export const API_BASE_URL = 'https://api-hospital.snlab.info/api';
 
 // Xử lý Media URL - loại bỏ /api ở cuối
 export const MEDIA_BASE_URL = API_BASE_URL.endsWith('/api')
